1e477e736f1ecf8a4db0ec96a7e87959feda3dc1
king <471897134@qq.com>
2018-12-16 12:32:01 +0800

    feat(all): bug fix

--- services/deploy.js
+++ services/deploy.js
@@ -6,13 +6,13 @@
 const fs = require('fs-extra');
 const path = require('path');
 const shell = require('shelljs');
-const deployDir = './deply';
+const deployDir = '../deply';
 
 module.exports = async (event) => {
     const branch  = event.payload.ref;
     const project = event.payload.repository.name;
     const message = event.payload.head_commit.message || '';
-    const deployPath = path.join(deployDir, `${project}.sh`);
+    const deployPath = path.join(__dirname, deployDir, `${project}.sh`);
     const exists = await fs.pathExists(deployPath);
     console.log('recive push message: ', message, deployPath, exists);
     if (exists && message.indexOf('feat(delpoy)') === 0) {
